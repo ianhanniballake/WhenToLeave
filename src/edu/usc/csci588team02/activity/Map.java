de09f49198702db113b0c6fdb93f73f0588f4523
Ian Lake <ian.hannibal.lake@gmail.com>
2010-10-09 10:00:20 -0700

    From Nory: Added ability to obtain user location from GPS.  GPS is the only form of user location supported by the emulator.
    Signed-off-by: Ian Lake <ian.hannibal.lake@gmail.com>

--- src/edu/usc/csci588team02/activity/Map.java
+++ src/edu/usc/csci588team02/activity/Map.java
@@ -2,9 +2,14 @@ package edu.usc.csci588team02.activity;
 
 import java.util.List;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
@@ -14,6 +19,8 @@ import com.google.android.maps.OverlayItem;
 
 import edu.usc.csci588team02.R;
 import edu.usc.csci588team02.maps.ItemizedOverlay;
+
+
 
 /**
  * @author Stephanie Trudeau
@@ -28,6 +35,8 @@ public class Map extends MapActivity
 	// List of all overlays on the map
 	List<Overlay> mapOverlays;
 	MapView mapView;
+    private LocationManager lm;
+    private LocationListener locationListener;
 
 	@Override
 	protected boolean isRouteDisplayed()
@@ -58,5 +67,60 @@ public class Map extends MapActivity
 		itemizedOverlay.addOverlay(overlayitem);
 		itemizedOverlay.addOverlay(overlayitem2);
 		mapOverlays.add(itemizedOverlay);
+		
+		//seattle
+		final GeoPoint point3 = new GeoPoint(47607428, -122327271);
+		final OverlayItem overlayitem3 = new OverlayItem(point3, "3", "3");
+		itemizedOverlay.addOverlay(overlayitem3);
+		
+        //---use the LocationManager class to obtain GPS locations---
+        lm = (LocationManager) 
+            getSystemService(Context.LOCATION_SERVICE);    
+        
+        locationListener = new MyLocationListener();
+        
+        lm.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER, 
+            0, 
+            0, 
+            locationListener);  
 	}
+	
+	
+    private class MyLocationListener implements LocationListener 
+    {
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, 
+            Bundle extras) {
+            // TODO Auto-generated method stub
+        }
+
+		@Override
+		public void onLocationChanged(Location location) {
+            if (location != null) {
+                Toast.makeText(getBaseContext(), 
+                    "Location changed : Lat: " + location.getLatitude() + 
+                    " Lng: " + location.getLongitude(), 
+                    Toast.LENGTH_SHORT).show();
+            
+            
+            
+        		GeoPoint point = new GeoPoint((int)(location.getLatitude()*1000000), (int)(location.getLongitude()*1000000));
+        		OverlayItem overlayitem = new OverlayItem(point, "", "");
+        		itemizedOverlay.addOverlay(overlayitem);
+            }
+			
+		}
+    } 
 }
