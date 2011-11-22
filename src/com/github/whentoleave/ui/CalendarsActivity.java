7954bac2198b8250488adc23272009f96000f873
Ian Lake <ian.hannibal.lake@gmail.com>
2011-11-21 21:06:36 -0800

    Reworks CalendarsActivity.java for GH-1.
    Signed-off-by: Ian Lake <ian.hannibal.lake@gmail.com>

--- src/com/github/whentoleave/ui/CalendarsActivity.java
+++ src/com/github/whentoleave/ui/CalendarsActivity.java
@@ -1,111 +1,58 @@
 package com.github.whentoleave.ui;
 
-import java.util.List;
-
 import android.app.ListActivity;
-import android.content.Context;
-import android.content.Intent;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.widget.ArrayAdapter;
-
-import com.github.whentoleave.model.CalendarEntry;
-import com.github.whentoleave.service.AppService;
-import com.github.whentoleave.service.AppServiceConnection;
+import android.provider.CalendarContract;
+import android.widget.AbsListView;
+import android.widget.CursorAdapter;
+import android.widget.SimpleCursorAdapter;
 
 /**
- * Sample for Google Calendar Data API using the Atom wire format to retrieve a
- * list of the user's calendars<br />
- * <br />
- * 
- * Modified from <a href=
- * "http://code.google.com/p/google-api-java-client/source/browse/calendar-v2-atom-android-sample/src/com/google/api/client/sample/calendar/android/CalendarAndroidSample.java?repo=samples"
- * >the google-api-java-client sample</a> by Yaniv Inbar
+ * Retrieves a simple list of all of the user's calendars
  */
-public final class CalendarsActivity extends ListActivity implements Handler.Callback
+public final class CalendarsActivity extends ListActivity implements
+		LoaderCallbacks<Cursor>
 {
 	/**
-	 * Connection to the persistent, authorized service
+	 * Adapter to display the list's data
 	 */
-	private final AppServiceConnection service = new AppServiceConnection(
-			new Handler(this));
-
-	/**
-	 * Handles an error from the AppService
-	 * 
-	 * @param errorMessage
-	 *            message to display
-	 */
-	private void handleError(final String errorMessage)
-	{
-		final String[] calendarNames = new String[] { errorMessage };
-		setListAdapter(new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, calendarNames));
-	}
-
-	/**
-	 * Handles a getCalendars event from the AppService
-	 * 
-	 * @param calendars
-	 *            newly returned calendars
-	 */
-	private void handleGetCalendars(final List<CalendarEntry> calendars)
-	{
-		final int numCalendars = calendars.size();
-		final String[] calendarNames = new String[numCalendars];
-		for (int i = 0; i < numCalendars; i++)
-			calendarNames[i] = calendars.get(i).title;
-		setListAdapter(new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, calendarNames));
-	}
-
-	@Override
-	public boolean handleMessage(final Message msg)
-	{
-		switch (msg.what)
-		{
-			case AppService.MSG_REFRESH_DATA:
-				handleRefreshData();
-				return true;
-			case AppService.MSG_GET_CALENDARS:
-				@SuppressWarnings("unchecked")
-				final List<CalendarEntry> calendars = (List<CalendarEntry>) msg.obj;
-				handleGetCalendars(calendars);
-				return true;
-			case AppService.MSG_ERROR:
-				final String errorMessage = (String) msg.obj;
-				handleError(errorMessage);
-				return true;
-			default:
-				return false;
-		}
-	}
-
-	/**
-	 * Handles a refreshData event from the AppService
-	 */
-	public void handleRefreshData()
-	{
-		service.requestCalendars();
-	}
+	private CursorAdapter adapter;
 
 	@Override
 	public void onCreate(final Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		getListView().setTextFilterEnabled(true);
-		bindService(new Intent(this, AppService.class), service,
-				Context.BIND_AUTO_CREATE);
-		startActivityForResult(new Intent(this, Login.class),
-				Login.REQUEST_AUTHENTICATE);
+		adapter = new SimpleCursorAdapter(this,
+				android.R.layout.simple_list_item_1, null,
+				new String[] { CalendarContract.Calendars.NAME },
+				new int[] { android.R.id.text1 }, 0);
+		setListAdapter(adapter);
+		getListView().setChoiceMode(AbsListView.CHOICE_MODE_NONE);
+		getLoaderManager().initLoader(0, null, this);
 	}
 
 	@Override
-	protected void onDestroy()
+	public Loader<Cursor> onCreateLoader(final int arg0, final Bundle arg1)
 	{
-		super.onDestroy();
-		service.unregister();
-		unbindService(service);
+		final String[] projection = { CalendarContract.Calendars.NAME };
+		return new CursorLoader(this, CalendarContract.Calendars.CONTENT_URI,
+				projection, null, null, CalendarContract.Calendars.NAME);
+	}
+
+	@Override
+	public void onLoaderReset(final Loader<Cursor> loader)
+	{
+		adapter.swapCursor(null);
+	}
+
+	@Override
+	public void onLoadFinished(final Loader<Cursor> loader, final Cursor data)
+	{
+		adapter.swapCursor(data);
 	}
 }
